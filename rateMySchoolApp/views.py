efbc0c588b7497eeba16015cf0e2f65e45215387
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-03-29 00:04:42 -0400

    render from the app

--- rateMySchoolApp/views.py
+++ rateMySchoolApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse # temp
+
 # Create your views here.
+def index(request):
+    """render the main page"""
+    return HttpResponse('<h1>Hello</h1>')
+    #return render(request,'<h1>Hello<\h1>')
