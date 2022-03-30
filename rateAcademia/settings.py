b2016ed6ac9e559ae476851217425f7cb2a992f4
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-03-29 20:37:57 -0400

    settings updated for csrf tocken

--- rateAcademia/settings.py
+++ rateAcademia/settings.py
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+CSRF_TRUSTED_ORIGINS=['https://rateacademia.herokuapp.com/']
