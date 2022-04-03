142a85226fd946f74061b35a1b4761e96dab42c0
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-04-03 18:04:31 -0400

    added Post and Profile models

--- rateMySchoolApp/views.py
+++ rateMySchoolApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Universities
+from .models import Universities, Post
 
 
 # Create your views here.
@@ -12,13 +12,29 @@ def index(request):
 from django.contrib.auth.models import User
 def college_rating(request):
     """renders college rating page"""
-    print(str(User.is_anonymous), "printing stuff")
     univeristies = Universities.objects.all()
-    print
+    
+    if 'collegeQuery' in request.GET:
+        q = request.GET['collegeQuery']
+        crude_data = Universities.objects.filter(name__icontains=q)
+        print(crude_data, "sfffff")
+        if len(crude_data) != 0: # if the search succeeds
+            query_post = Post.objects.filter(ratedBody=crude_data[0])
+            # debug
+            # print(crude_data[0])
+            # print(query_post, len(query_post), "query post")
+            data = crude_data[0]
+            #summary = get_summary(data)
+            #print("data")
+            
+        
+            
+
     context = {
         'universities' : univeristies,
     }
     return render(request, 'rateMySchool/collegeRating.html', context)
+
 #@login_required #, if necessary
 def dashboard(request):
     return render(request, 'rateMySchool/dashboard.html')
