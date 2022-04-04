a45f63667c6f028482f2ed8c9286e0f09cf52f63
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-04-03 21:14:05 -0400

    added models and ready for deployment

--- rateMySchoolApp/views.py
+++ rateMySchoolApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Universities, Post
-
+import wikipediaapi
 
 # Create your views here.
 def index(request):
@@ -9,29 +9,86 @@ def index(request):
     #return HttpResponse('<h1>Hello</h1>')
     return render(request,'rateMySchool/index.html')
 
-from django.contrib.auth.models import User
+def get_summary(name):
+    """takes the title of university and returns a wiki summery"""
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+    page_py = wiki_wiki.page(name)
+    if page_py.exists():
+        return page_py.summary
+    else:
+        return 'Wiki summary not found'
+
+def matchRatings(data):
+    """matches ratings data to lables"""
+    matchedData = []
+    lable = []
+    lable.append("5-star")
+    matchedData.append(data.count(5))
+    lable.append("4-star")
+    matchedData.append(data.count(4))
+    lable.append("3-star")
+    matchedData.append(data.count(3))
+    lable.append("2-star")
+    matchedData.append(data.count(2))
+    lable.append("1-star")
+    matchedData.append(data.count(1))
+    
+    return lable, matchedData
+
+def Average(lst):
+    """calculate average rating"""
+    if len(lst) > 0:
+        return sum(lst) / len(lst)
+    else:
+        return 0
+
 def college_rating(request):
     """renders college rating page"""
+    # Todo: make sure that one user can't rate a university more than once
+    #     : numerical and text ratings should be overwritten
+    summary = ''
+    searchedUniversity = ''
+    labledRatings = ''
+    lable = ''
+    average_rating = ''
+    graph_data = []
     univeristies = Universities.objects.all()
-    
     if 'collegeQuery' in request.GET:
         q = request.GET['collegeQuery']
         crude_data = Universities.objects.filter(name__icontains=q)
-        print(crude_data, "sfffff")
         if len(crude_data) != 0: # if the search succeeds
-            query_post = Post.objects.filter(ratedBody=crude_data[0])
+            # find posts related to university (formerly called query_post)
+            universityPostRatings = Post.objects.filter(ratedBody=crude_data[0])
             # debug
             # print(crude_data[0])
             # print(query_post, len(query_post), "query post")
-            data = crude_data[0]
-            #summary = get_summary(data)
-            #print("data")
+            # searched university (formerly called data)
+            searchedUniversity = crude_data[0] # pick first
+            summary = get_summary(searchedUniversity)
+            
+            # later should be ordered by users badge (Gold, Silver, Platinium)
+            universityRatePosts = Post.objects.filter(ratedBody=searchedUniversity).order_by('-rate_stars')
+            for post in universityRatePosts:
+                graph_data.append(post.rate_stars)
+            
+            average_rating = Average(graph_data)
+            # labled Ratings formerly graph_data
+            lable, labledRatings = matchRatings(graph_data)
+
+            print(average_rating, lable, labledRatings)
             
         
             
 
     context = {
         'universities' : univeristies,
+        'wiki_summary': summary,
+        'universities' : univeristies,
+        'queryUNI' : searchedUniversity,
+        # chart data
+        'graph_data': labledRatings,
+        'lable': lable,
+        'average_rating': average_rating,
     }
     return render(request, 'rateMySchool/collegeRating.html', context)
 
