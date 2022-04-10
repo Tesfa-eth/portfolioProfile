8fffd9302b0465aabf4f1ce66ac081dca0e377f3
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-04-09 21:19:03 -0400

    added My ratings page!

--- rateMySchoolApp/urls.py
+++ rateMySchoolApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='ratemySchool-index'),
     path('collegeRating/', views.college_rating, name='collegeRating'),
-    path("dashboard/", views.dashboard, name="dashboard"), # new
-    path("accounts/", include("allauth.urls")), # new
+    path("dashboard/", views.dashboard, name="dashboard"), 
+    path("myratings/", views.myRatings, name="myratings"), 
+    path("accounts/", include("allauth.urls")),
 ]
