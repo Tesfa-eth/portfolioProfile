504836477e3f801362df0e0f632ea146b3f0fb53
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-05-16 03:51:18 -0400

    bug fixed and added some styling.

--- rateAcademia/settings.py
+++ rateAcademia/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
